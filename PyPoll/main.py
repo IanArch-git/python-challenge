cfdca32ed949d6e13dc029a161d24b7e9a53edbd
Ian Arch <ian.arch01@gmail.com>
2021-02-08 21:10:29 -0700

    Total Votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,26 @@
 # * You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
 
+total_votes = 0
+
+
 import csv
 file_path = "election_data.csv"
 
 with open(file_path) as csvfile:
     csvreader = csv.reader(csvfile)
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
+    print(f"CSV Header: {csv_header}")
 
     VoterID = csv_header.index('Voter ID')
     County = csv_header.index('County')
     Candidate = csv_header.index('Candidate')
 
+    for row in csvreader:
+        total_votes = total_votes + 1
     
+    print(f"Total Votes: {total_votes}")
+        
+        #total_pl = total_pl + int(row[PL])
 
 #   * The total number of votes cast
 
