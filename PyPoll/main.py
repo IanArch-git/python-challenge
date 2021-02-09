3b81e6d59b20e672f4bf54d6d26987a3db714bf1
Ian Arch <ian.arch01@gmail.com>
2021-02-09 15:04:28 -0700

    formatting and beginning to find winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -41,15 +41,27 @@ with open(file_path) as csvfile:
     cand4 = unique_candz[3]
     cand4_count = candz.count(cand4)
 
-     
+    cand1_percent = round((cand1_count / total_votes)*100)
+    cand2_percent = round((cand2_count / total_votes)*100)
+    cand3_percent = round((cand3_count / total_votes)*100)
+    cand4_percent = round((cand4_count / total_votes)*100)
+
+    maxList = [cand1_percent, cand2_percent, cand3_percent, cand4_percent]
+    
+
     #print(unique_candz)
 
+    print(f"Election Results")
+    print("-------------------------")
     print(f"Total Votes: {total_votes}")
-    print(f"{cand1}: {cand1_count}")
-    print(f"{cand2}: {cand2_count}")
-    print(f"{cand3}: {cand3_count}")
-    print(f"{cand4}: {cand4_count}") 
-
+    print("-------------------------")
+    print(f"{cand1}: {cand1_percent}.000% ({cand1_count})")
+    print(f"{cand2}: {cand2_percent}.000% ({cand2_count})")
+    print(f"{cand3}: {cand3_percent}.000% ({cand3_count})")
+    print(f"{cand4}: {cand4_percent}.000% ({cand4_count})")
+    print("-------------------------")
+    #print(Winner)
+    #print("-------------------------")
 
 #   * A complete list of candidates who received votes
 
